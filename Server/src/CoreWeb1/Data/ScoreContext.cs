35980039801dfedd1a2edd65a719e3c461654dc4
Nicholas Ventimiglia <nick@avariceonline.com>
2016-12-03 19:33:05 -0800

    Update ScoreContext.cs

--- Server/src/CoreWeb1/Data/ScoreContext.cs
+++ Server/src/CoreWeb1/Data/ScoreContext.cs
@@ -14,6 +14,7 @@ namespace CoreWeb1.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // TODO move to startup
             optionsBuilder.UseSqlite("Filename=./score.db");
         }
     }
